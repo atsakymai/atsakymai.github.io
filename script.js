c0ad9c1753aba0115acd82f46f73898d4a5444fd
atsakymai <147638953+atsakymai@users.noreply.github.com>
2023-11-14 18:35:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bookButton = document.getElementById("book");
 const homeButton = document.getElementById("home");
-const homeworkButton = document.getElementById("homework");
+const learnButton = document.getElementById("learn");
+const Slider = document.getElementById("Slider");
+const SeeAll = document.getElementById("SeeAll")
 
 
 
@@ -9,8 +11,8 @@ bookButton.onclick = function(){
     bookButton.style.color = "#fff";
     homeButton.style.background = "transparent";
     homeButton.style.color = "#d4d5d9";
-    homeworkButton.style.background = "transparent";
-    homeworkButton.style.color = "#d4d5d9";
+    learnButton.style.background = "transparent";
+    learnButton.style.color = "#d4d5d9";
 }
 
 homeButton.onclick = function(){
@@ -18,15 +20,15 @@ homeButton.onclick = function(){
     bookButton.style.color = "#d4d5d9";
     homeButton.style.background = "#ec256e";
     homeButton.style.color = "#fff";
-    homeworkButton.style.background = "transparent";
-    homeworkButton.style.color = "#d4d5d9";
+    learnButton.style.background = "transparent";
+    learnButton.style.color = "#d4d5d9";
 }
 
-homeworkButton.onclick = function(){
+learnButton.onclick = function(){
     bookButton.style.background = "transparent";
     bookButton.style.color = "#d4d5d9";
     homeButton.style.background = "transparent";
     homeButton.style.color = "#d4d5d9";
-    homeworkButton.style.background = "#ec256e";
-    homeworkButton.style.color = "#fff";
+    learnButton.style.background = "#ec256e";
+    learnButton.style.color = "#fff";
 }
